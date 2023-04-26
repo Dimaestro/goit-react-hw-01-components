c097b3e036cdef20a9255be0b64863a25713cbd3
Dima <vidistudioproduction@gmail.com>
2023-04-26 15:37:14 +0300

    update friendList

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,10 @@
-import clsx from 'clsx';
+import clsx from "clsx";
 import style from './FriendListItem.module.css';
 
-const FriendListItem = ({ name, avatar, isOnline }) => {
+const FriendListItem = ({name, avatar, isOnline}) => {
   return (
     <li className={style.item}>
-      <span className={clsx(style.status, isOnline ? style.active : '')}></span>
+      <span className={clsx(style.status , isOnline && style.active)}></span>
       <img className={style.avatar} src={avatar} alt={name} width="48" />
       <p className={style.name}>{name}</p>
     </li>
